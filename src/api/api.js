6e386dd335ddb3949516c00b87896d42a9443ac8
le0piovesan <leopvsn@gmail.com>
2022-06-08 15:41:30 -0300

    allowing cors

--- src/api/api.js
+++ src/api/api.js
@@ -1,20 +1,33 @@
 const express = require("express");
+var cors = require("cors");
 const app = express();
 const model = require("./model");
+
+app.use(cors());
+
 app.use(express.json());
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+app.use((req, res, next) => {
+  const allowedOrigins = [
+    "http://localhost:8888",
+    "https://qualicorp-hiring-test.netlify.app",
+  ];
+  const origin = req.headers.origin;
+  res.setHeader("Access-Control-Allow-Origin", origin);
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, OPTIONS, PATCH, DELETE, POST, PUT"
   );
-  next();
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  return next();
 });
 
 const port = process.env.PORT || 8888;
 
 app.get("/", async (req, res) => {
+  res.setHeader();
   const result = await model.findAll();
   res.json(result);
 });
