76b3095bf495b91c4fedd63f98fcfbdf83137783
le0piovesan <leopvsn@gmail.com>
2022-06-08 14:34:09 -0300

    trying to fix cors

--- src/api/api.js
+++ src/api/api.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const app = express();
 const model = require("./model");
-var cors = require("cors");
 
-app.use(cors());
-
-app.use(
-  cors({
-    origin:
-      "http://localhost:3000" || "https://qualicorp-hiring-test.netlify.app/",
-    credentials: true,
-  })
-);
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested, Content-Type, Accept Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE");
+    return res.status(200).json({});
+  }
+  next();
+});
 
 app.use(express.json());
 
